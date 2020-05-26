b0cbacf45bd653ae3e36cfa6a58305938e5c336f
jasonxjung <jasonjung.xc@gmail.com>
2020-05-26 22:20:43 +0300

    recursion final solution

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 function isEven(num){
     if (num == 0 ){
-    return true;
-    } else if (num == 1){
-        return false;
-    } else if ((num - 2)% 2 == 0 && num > 0){
         return true;
-    } else {
+    } else if (num == 1 || num < 0){
         return false;
     }
+    else 
+    {
+        num = num - 2;
+        return isEven(num);
+    } 
 }
 
-console.log("the number entered is even number "+isEven(50));
+console.log("the number entered is even number "+ isEven(12));

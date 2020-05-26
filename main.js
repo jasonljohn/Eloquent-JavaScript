4e79befcab5eed07ba03a9d98ffcd1ee21c89f0d
jasonxjung <jasonjung.xc@gmail.com>
2020-05-26 21:44:35 +0300

    mminimum solution

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-function chessboard(n) {
-    let i,j;
-    let board = "";
-        for (i=0;i<n;i++){
-            for (j=0;j<n;j++){
-                if ((i+j)%2 == 0){
-                    //document.write(" ");
-                    //console.log(" ");
-                    board += " ";
-                } else {
-                    //document.write("#");
-                    //console.log("#");
-                    board += "#";
-                }
-            }
-            //console.log("\n");
-            //document.write("<br >");
-            board += "\n";
-        }
-        console.log(board);
-        //document.write(board);
+function min(a,b) {
+    if (a < b){
+        return a;
+    } else {
+        return b;
     }
-chessboard(8);
+}
+
+console.log(min(10,45));
